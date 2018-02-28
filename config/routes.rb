756f1c172ff1bbb74c7cbc2b27246a89608604a8
spy <sp831031@gmail.com>
2018-02-28 12:29:23 +0800

    setup routes for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "products#index"
+  namespace :admin do
+    root "products#index"
+    resource :products ,only:[:index, :edit, :update, :destroy]
+
+  end
+  resource :products ,only:[:index, :show]
+
 end
