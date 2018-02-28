ce748135d32faf45bc8bd3922f98eb3075240f44
spy <sp831031@gmail.com>
2018-02-28 16:45:14 +0800

    add create action to product controller for admin user

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,5 +2,23 @@ class Admin::ProductsController < ApplicationController
 
   def index 
     @products = Product.page(params[:page]).per(10)
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+       flash[:notice] = "product was successfully created"
+      redirect_to admin_products_path
+    else
+      flash.now[:alert] = "restaurant was failed to created"
+      render :index
+    end
+  end
+
+  private 
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :image)
   end
 end
