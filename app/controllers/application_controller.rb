7b38efa7ca1a526cec0188b301c57beaf26c795f
spy <sp831031@gmail.com>
2018-03-01 12:29:18 +0800

    add current_cart and set cart method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_cart
+
+  private
+
+  def current_cart
+    @cart || set_cart
+  end
+
+  def set_cart
+
+    if session[:cart_id]
+      @cart = Cart.find_by(id: session[:cart_id])
+    end
+
+    @cart ||= Cart.create
+
+    session[:cart_id] = @cart.id
+    @cart
+  end
 
   def authenticate_admin
     unless current_user.admin?
@@ -8,4 +27,6 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+
+
 end
