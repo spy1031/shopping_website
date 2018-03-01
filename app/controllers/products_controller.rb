be3f065deafaad297b484d618b3316bf073b8a3a
spy <sp831031@gmail.com>
2018-03-01 14:31:32 +0800

    fix some bug to make add_to_cart function work

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,13 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.page(params[:page]).per(10)
+    @cart_items = current_cart.cart_items
+    if @cart_items != nil
+      @cart_products = Array.new
+      @cart_items.each do |item|
+        @cart_products << item.product 
+      end
+    end
   end
 
   def add_to_cart
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,16 +1,17 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
-end
 
-
-def add_cart_item(product)
-  existing_item =self.cart_items.find_by(product_id: product)
-  if existing_item
-    existing_item.quantity+=1
-    existing_item.save!
-  else
-    cart_item = cart_items.build(product_id: product)
-    cart_item.save!
+  def add_cart_item(product)
+    existing_item =self.cart_items.find_by(product_id: product.id)
+    if existing_item
+      existing_item.quantity+=1
+      existing_item.save!
+    else
+      cart_item = self.cart_items.build(product_id: product.id)
+      cart_item.save!
+    end
+    self.cart_items
   end
-  self.cart_items
 end
+
+
