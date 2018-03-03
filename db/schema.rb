6463769ccb70b21ee3b827fda59642b0a002900a
spy <sp831031@gmail.com>
2018-03-03 15:08:13 +0800

    add email attribute to order table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303022452) do
+ActiveRecord::Schema.define(version: 20180303070627) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20180303022452) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "products", force: :cascade do |t|
