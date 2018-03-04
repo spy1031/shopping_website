174ed8f299c93b1f8a44ba4f8115c660c78a8203
spy <sp831031@gmail.com>
2018-03-04 16:01:35 +0800

    add payment_method attribute to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303070627) do
+ActiveRecord::Schema.define(version: 20180304075958) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20180303070627) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "payment_method"
   end
 
   create_table "products", force: :cascade do |t|
