a76d8c120a83396c76567e0b2c67fe6d46092f9c
spy <sp831031@gmail.com>
2018-03-26 16:55:11 +0800

    update order edit form and update function for admin user

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,4 +6,12 @@ class Admin::OrdersController < ApplicationController
   def edit
     @order = Order.find(params[:id])
   end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.shipping_status = params[:shipping_status]
+    @order.payment_status =params[:payment_status]
+    @order.save!
+    redirect_to admin_orders_path
+  end
 end
