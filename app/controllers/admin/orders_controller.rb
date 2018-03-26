e18f6e29e28c5e1794cce3863135f34043a7926c
spy <sp831031@gmail.com>
2018-03-26 16:27:39 +0800

    add edit order page for admin user

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,4 +2,8 @@ class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.page(params[:page]).per(10)
   end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
 end
