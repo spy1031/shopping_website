3e6033834bdbb0fb2f2f8b25f9859f1c0c3d9729
spy <sp831031@gmail.com>
2018-03-26 17:27:37 +0800

    update email setting for environments production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,9 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "shopping_website_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: "https://my-shopping-website.herokuapp.com/" }
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
